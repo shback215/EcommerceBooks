6ef24c309cdcdb20cf73d153b58cc13ef15b7cfa
shback215 <shbackup2252@gmail.com>
2026-05-15 14:06:30 -0400

    Fix CSS syntax in App.css

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -8,9 +8,9 @@ function About() {
       <div>
         <p className="justify">This website provides descriptions of books by Stanley B. Howard that are available
           on Amazon Books. These books are:</p>
-          <p className="bullet-item">&bull; <i>When the Dogwood Blooms: Seasons in a Boys Life</i></p>
-          <p className="bullet-item">&bull; <i>Icarus: An American Odyssey</i></p>
-          <p className="bullet-item">&bull; <i>Me: Jack in the Box with OCD</i></p>
+          <p className="bullet-item">&bull; <i>When the Dogwood Blooms: Seasons in a Boys Life</i>: A description of this novel is at <Link to="/about-dogwood">About Dogwood</Link></p>
+          <p className="bullet-item">&bull; <i>Icarus: An American Odyssey</i>: A description of this novel is at <Link to="/about-icarus">About Icarus</Link></p>
+          <p className="bullet-item">&bull; <i>Me: Jack in the Box with OCD</i>: A description of this novel is at <Link to="/about-jack">About Jack</Link></p>
       </div>
       <nav>
         <Link to="/">Back to Home</Link>

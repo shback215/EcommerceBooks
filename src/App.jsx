63d8b1bea1178448622dca22f110f4346241004f
shback215 <shbackup2252@gmail.com>
2026-05-14 23:05:37 -0400

    Add review workflow and update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { Routes, Route, Link } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
 import AboutDogwood from './pages/AboutDogwood'
+import AboutIcarus from './pages/AboutIcarus'
+import AboutJack from './pages/AboutJack'
 
 import './CSS/App.css'
 
@@ -12,13 +14,17 @@ function App() {
       <nav className="nav-links">
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
-        <Link to="/about-dogwood">AboutDogwood</Link>
+        <Link to="/about-dogwood">About Dogwood</Link>
+        <Link to="/about-icarus">About Icarus</Link>
+        <Link to="/about-jack">About Jack</Link>
       </nav>
       
      <Routes>
         <Route path="/" element={<Home />} />        
         <Route path="/about" element={<About />} />
-        <Route path="/about-dogwood" element={<AboutDogwood />} />
+        <Route path="/about-dogwood" element={<AboutDogwood />} />        
+        <Route path="/about-icarus" element={<AboutIcarus />} />
+        <Route path="/about-jack" element={<AboutJack />} />
         <Route path="*" element={<Home />} />
       </Routes>
     </div>
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -7,7 +7,7 @@ function About() {
       <h2>About Books by Stanley B. Howard</h2>
       <div>
         <p className="justify">This website provides descriptions of books by Stanley B. Howard that are available
-          on Amazon Books. These books are:.</p>
+          on Amazon Books. These books are:</p>
           <p className="bullet-item">&bull; <i>When the Dogwood Blooms: Seasons in a Boys Life</i></p>
           <p className="bullet-item">&bull; <i>Icarus: An American Odyssey</i></p>
           <p className="bullet-item">&bull; <i>Me: Jack in the Box with OCD</i></p>
